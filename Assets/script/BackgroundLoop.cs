a6d9f65694ed26b7805108e3729d4c40f88357f8
JeongYeonKim <95961799+blueconecell@users.noreply.github.com>
2022-11-29 20:32:02 +0900

    tilemap cloud

--- Assets/script/BackgroundLoop.cs
+++ Assets/script/BackgroundLoop.cs
@@ -7,26 +7,29 @@ public class BackgroundLoop : MonoBehaviour
 
     // 배경가로길이
     private float width;
+    public float spawnPoint = 0;
+    public float endPoint = 0;
 
 
     private void Awake()
     {
-        BoxCollider2D backgroundCollider = GetComponent<BoxCollider2D>();
-        width = backgroundCollider.size.x;
+        //BoxCollider2D backgroundCollider = GetComponent<BoxCollider2D>();
+        //width = backgroundCollider.size.x;
     }
    
 
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.x <= -width)
+        if (transform.position.x <= endPoint)
         {
             Reposition();
         }
     }
     private void Reposition()
     {
-        Vector2 offset = new Vector2(width * 2f, 0);    
-        transform.position = (Vector2)transform.position + offset;
+        //Vector2 offset = new Vector2(width * 2f+spawnPoint, 0);
+        Vector3 offset = new Vector3(spawnPoint,15, 0);
+        transform.position = offset;
     }
 }
